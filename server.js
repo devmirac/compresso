7408a97f0b792f3bb685103a408b8790a3f3e6d3
Miraç <61243523+lilmirac@users.noreply.github.com>
2024-09-08 22:48:00 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+  next();
+});
+
+// Move this line before the route handler
+app.use(express.static(path.join(__dirname, '')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
